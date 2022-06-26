1537fdfb18a5b1ad4edc5875cd6b293bf0576e04
juancoob <ja.cobos.obrero@gmail.com>
2022-06-26 18:18:41 +0200

    feat: Update server quiz model

--- app/src/main/java/com/juancoob/kahootchallenge/data/server/models/Quiz.kt
+++ app/src/main/java/com/juancoob/kahootchallenge/data/server/models/Quiz.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Quiz(
+    val uuid: String,
     val title: String,
     val description: String,
     val questions: List<Question>,
