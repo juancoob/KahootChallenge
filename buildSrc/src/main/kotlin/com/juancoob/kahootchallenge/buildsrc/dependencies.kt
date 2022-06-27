73bdc22497368a8f6573a65c6f789a070d2740e6
juancoob <ja.cobos.obrero@gmail.com>
2022-06-27 13:13:10 +0200

    fix: Add remaining Hilt dependencies

--- buildSrc/src/main/kotlin/com/juancoob/kahootchallenge/buildsrc/dependencies.kt
+++ buildSrc/src/main/kotlin/com/juancoob/kahootchallenge/buildsrc/dependencies.kt
@@ -73,6 +73,7 @@ object Libs {
         private const val version = "2.42"
         const val hiltAndroid = "com.google.dagger:hilt-android:$version"
         const val hiltCompiler = "com.google.dagger:hilt-compiler:$version"
+        const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
     }
 
     object Javax {
