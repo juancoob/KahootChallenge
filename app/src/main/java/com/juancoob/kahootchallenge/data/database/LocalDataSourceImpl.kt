e20db299dd862ee94242774cd8376312869bd0c2
juancoob <ja.cobos.obrero@gmail.com>
2022-07-18 12:19:24 +0200

    refactor: Update LocalDataSourceImpl with the DbLocalMapper

--- app/src/main/java/com/juancoob/kahootchallenge/data/database/LocalDataSourceImpl.kt
+++ app/src/main/java/com/juancoob/kahootchallenge/data/database/LocalDataSourceImpl.kt
@@ -1,36 +1,48 @@
 package com.juancoob.kahootchallenge.data.database
 
 import com.juancoob.data.datasource.LocalDataSource
-import com.juancoob.domain.Choice
 import com.juancoob.domain.ErrorRetrieved
-import com.juancoob.domain.Question
 import com.juancoob.domain.Quiz
-import com.juancoob.kahootchallenge.data.database.models.QuestionWithChoices
-import com.juancoob.kahootchallenge.data.database.models.QuizWithQuestions
 import com.juancoob.kahootchallenge.data.tryCall
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import com.juancoob.kahootchallenge.data.database.models.Choice as DbChoice
-import com.juancoob.kahootchallenge.data.database.models.Question as DbQuestion
-import com.juancoob.kahootchallenge.data.database.models.Quiz as DbQuiz
 
 class LocalDataSourceImpl @Inject constructor(
-    private val quizDao: QuizDao
+    private val quizDao: QuizDao,
+    private val dbLocalMapper: DbLocalMapper
 ) : LocalDataSource {
+
+    private var questionIndex: Int = -1
+    private var choiceIndex: Int = -1
+
     override suspend fun isEmpty(): Boolean =
         quizDao.getQuizCount() == 0
 
     override suspend fun insertQuizData(quiz: Quiz): ErrorRetrieved? = tryCall {
-        quizDao.insertQuiz(quiz.fromLocalModel())
+        quizDao.insertQuiz(with(dbLocalMapper) { quiz.fromLocalModel() })
 
         for (question in quiz.questions) {
-            val questionIndex = quiz.questions.indexOf(question)
-            quizDao.insertQuestion(question.fromLocalModel(quiz.uuid, questionIndex))
+            questionIndex = quiz.questions.indexOf(question)
+            quizDao.insertQuestion(
+                with(dbLocalMapper) {
+                    question.fromLocalModel(
+                        quiz.uuid,
+                        questionIndex
+                    )
+                }
+            )
 
             for (choice in question.choices) {
-                val choiceIndex = question.choices.indexOf(choice)
-                quizDao.insertChoice(choice.fromLocalModel(questionIndex, choiceIndex))
+                choiceIndex = question.choices.indexOf(choice)
+                quizDao.insertChoice(
+                    with(dbLocalMapper) {
+                        choice.fromLocalModel(
+                            questionIndex,
+                            choiceIndex
+                        )
+                    }
+                )
             }
         }
     }.fold(
@@ -39,57 +51,5 @@ class LocalDataSourceImpl @Inject constructor(
     )
 
     override fun getQuiz(): Flow<Quiz> =
-        quizDao.getQuiz().map { it.toLocalModel() }
+        quizDao.getQuiz().map { with(dbLocalMapper) { it.toLocalModel() } }
 }
-
-fun Quiz.fromLocalModel(): DbQuiz = DbQuiz(
-    uuid = uuid,
-    title = title,
-    description = description,
-    quizType = quizType
-)
-
-fun Question.fromLocalModel(quizId: String, questionId: Int) = DbQuestion(
-    id = questionId,
-    quizId = quizId,
-    type = type,
-    image = image,
-    question = question,
-    pointsMultiplier = pointsMultiplier,
-    time = time
-)
-
-fun Choice.fromLocalModel(questionId: Int, choiceId: Int) = DbChoice(
-    choiceId = choiceId,
-    questionId = questionId,
-    answer = text,
-    correct = isCorrect
-)
-
-fun QuizWithQuestions.toLocalModel() = quiz.run {
-    Quiz(
-        uuid = uuid,
-        quizType = quizType,
-        title = title,
-        description = description,
-        questions = questions.map { it.toLocalModel() }
-    )
-}
-
-fun QuestionWithChoices.toLocalModel() = question.run {
-    Question(
-        type = type,
-        image = image,
-        question = question,
-        choices = choices.map { it.toLocalModel() },
-        pointsMultiplier = pointsMultiplier,
-        time = time
-    )
-}
-
-fun DbChoice.toLocalModel() = Choice(
-    text = answer,
-    isCorrect = correct,
-    showAnswer = false,
-    isSelected = false
-)
